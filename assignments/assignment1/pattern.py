a89b8b4c71cc30250e9ae7b9c50c549a9b017eec
Mr-RitikSingh <ritiksingh-06>
2024-12-19 15:53:53 +0530

    pattern file added into assignment1 folder

---
+++ assignments/assignment1/pattern.py
@@ -0,0 +1,14 @@
+# The TASK is:-
+
+# >  file name must be pattern.py in which you are going to write the code.
+# >  commit this code on your github link under pythonbasic branch. 
+
+
+# ***********************************************************************************************************
+
+
+i = 9
+while i>0:
+    x = str(i)
+    print(i*x)
+    i = i-1
